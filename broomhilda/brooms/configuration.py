b543c3c3228579cf86c4bf6f259de1f8303e97ed
adontz <adontz@gmail.com>
2019-07-13 22:25:35 +0400

    Rewrite configuration parsing.

--- broomhilda/brooms/configuration.py
+++ broomhilda/brooms/configuration.py
@@ -1,122 +1,151 @@
-from collections import defaultdict
-from collections import namedtuple
-from json import load as json_load
-from os import scandir
-from os.path import abspath
-from os.path import join
-from yaml import safe_load as yaml_load
+class _ConfigurationFile:
+    __slots__ = 'absolute_path', 'relative_path', 'size', 'atime', 'mtime', 'ctime'
+
+    def __init__(self, absolute_path, relative_path, size, atime, mtime, ctime):
+        self.absolute_path = absolute_path
+        self.relative_path = relative_path
+        self.size = size
+        self.atime = atime
+        self.mtime = mtime
+        self.ctime = ctime
+
+    def read(self):
+        from json import load as json_load
+        from yaml import safe_load as yaml_load
+
+        # Any meaningful config file is larger than 4 bytes
+        # So we filter out empty files or files with single \r\n
+        if self.size > 4:
+            if self.absolute_path.endswith('.json'):
+                with open(self.absolute_path, 'r') as file:
+                    return json_load(file)
+            elif self.absolute_path.endswith('.yaml'):
+                with open(self.absolute_path, 'r') as file:
+                    return yaml_load(file)
+
+        return None
 
 
-_FileEntry = namedtuple('_FileEntry', ('absolute_path', 'relative_path', 'size', 'atime', 'mtime', 'ctime'))
+class _ConfigurationLayer:
+    def _scan_(self, absolute_path, relative_path):
+        from os import scandir
+        from os.path import join
+
+        directory_entries = []
+        file_entries = []
+
+        for entry in scandir(absolute_path):
+            if entry.is_dir():
+                directory_entries.append(entry)
+            elif entry.is_file():
+                file_entries.append(entry)
+
+        file_entries = sorted(file_entries, key=lambda e: e.name)
+        directory_entries = sorted(directory_entries, key=lambda e: e.name)
+
+        for entry in file_entries:
+            entry_absolute_path = join(absolute_path, entry.name)
+            entry_relative_path = join(relative_path, entry.name)
+            stat = entry.stat()
+            self.file_entries.append(
+                _ConfigurationFile(
+                    entry_absolute_path,
+                    entry_relative_path,
+                    stat.st_size,
+                    stat.st_atime,
+                    stat.st_mtime,
+                    stat.st_ctime))
+
+        for entry in directory_entries:
+            entry_absolute_path = join(absolute_path, entry.name)
+            entry_relative_path = join(relative_path, entry.name)
+            self._scan_(entry_absolute_path, entry_relative_path)
+
+    def __init__(self, absolute_path):
+        from os.path import abspath
+
+        self.absolute_path = absolute_path
+        self.file_entries = []
+        self._scan_(abspath( self.absolute_path), '')
 
 
-def _scandir_recursive(entries, absolute_path, relative_path):
-    directory_entries = []
-    file_entries = []
+class Configuration:
+    def _merge_dict(self, lower_dict):
+        def _merge_dict_recursive(upper_dict, upper_dict_priority, lower_dict, lower_dict_priority):
+            for item_key, lower_item in lower_dict.items():
+                upper_item = upper_dict.get(item_key)
 
-    for entry in scandir(absolute_path):
-        if entry.is_dir():
-            directory_entries.append(entry)
-        elif entry.is_file():
-            file_entries.append(entry)
+                if (type(lower_item) is dict) and (type(upper_item) is dict):
+                    should_reset = False
 
-    file_entries = sorted(file_entries, key=lambda e: e.name)
-    directory_entries = sorted(directory_entries, key=lambda e: e.name)
+                    for key, value in upper_item.items():
+                        if (key is None) and (value is None):
+                            should_reset = True
+                        break
 
-    for entry in file_entries:
-        entry_absolute_path = join(absolute_path, entry.name)
-        entry_relative_path = join(relative_path, entry.name)
-
-        stat = entry.stat()
-        entries.append(
-            _FileEntry(
-                entry_absolute_path,
-                entry_relative_path,
-                stat.st_size,
-                stat.st_atime,
-                stat.st_mtime,
-                stat.st_ctime))
-
-    for entry in directory_entries:
-        entry_absolute_path = join(absolute_path, entry.name)
-        entry_relative_path = join(relative_path, entry.name)
-        _scandir_recursive(entries, entry_absolute_path, entry_relative_path)
-
-
-def _merge_dict(upper_dict, upper_dict_priority, lower_dict, lower_dict_priority):
-    if upper_dict_priority < lower_dict_priority:
-        for item_key, lower_item in lower_dict.items():
-            upper_item = upper_dict.get(item_key)
-
-            if (type(lower_item) is dict) and (type(upper_item) is dict):
-                upper_item_priority = upper_item.get('__priority', upper_dict_priority)
-                lower_item_priority = lower_item.get('__priority', lower_dict_priority)
-                _merge_dict(upper_item, upper_item_priority, lower_item, lower_item_priority)
-            elif (type(lower_item) is list) and (type(upper_item) is list):
-                upper_dict[item_key] = lower_item + upper_item
-            else:
-                upper_dict[item_key] = lower_item
-    else:
-        for item_key, lower_item in lower_dict.items():
-            upper_item = upper_dict.get(item_key)
-
-            if (type(lower_item) is dict) and (type(upper_item) is dict):
-                upper_item_priority = upper_item.get('__priority', upper_dict_priority)
-                lower_item_priority = lower_item.get('__priority', lower_dict_priority)
-                _merge_dict(upper_item, upper_item_priority, lower_item, lower_item_priority)
-            elif (type(lower_item) is list) and (type(upper_item) is list):
-                upper_dict[item_key] = lower_item + upper_item
-            else:
-                if not item_key in upper_dict:
-                    upper_dict[item_key] = lower_item
-
-
-def _clear_dict(dirty_dict):
-    for key in [k for k in dirty_dict.keys() if (type(k) is str) and k.startswith('__')]:
-        del dirty_dict[key]
-
-    for key, item in dirty_dict.items():
-        if type(item) is dict:
-            _clear_dict(item)
-
-
-def load(paths):
-    layers = []
-    file_data = defaultdict(dict)
-    file_skip = set()
-
-    for path in paths:
-        entries = []
-
-        _scandir_recursive(entries, abspath(path), '')
-
-        layers.append(entries)
-
-    for layer in reversed(layers):
-        for file_entry in layer:
-            if not file_entry.relative_path in file_skip:
-                upper_data = file_data[file_entry.relative_path]
-
-                if file_entry.size <= 4:
-                    file_skip.add(file_entry.relative_path)
-                else:
-                    if file_entry.absolute_path.endswith('.json'):
-                        with open(file_entry.absolute_path, 'r') as file:
-                            lower_data = json_load(file)
-                    elif file_entry.absolute_path.endswith('.yaml'):
-                        with open(file_entry.absolute_path, 'r') as file:
-                            lower_data = yaml_load(file)
+                    if should_reset:
+                        del upper_item[None]
                     else:
-                        lower_data = None
+                        upper_item_priority = upper_item.get('__priority', upper_dict_priority)
+                        lower_item_priority = lower_item.get('__priority', lower_dict_priority)
+                        _merge_dict_recursive(upper_item, upper_item_priority, lower_item, lower_item_priority)
+                elif (type(lower_item) is list) and (type(upper_item) is list):
+                    if upper_item:
+                        if upper_item[0] is None:
+                            upper_dict[item_key] = upper_item[1:]
+                        else:
+                            upper_dict[item_key] = lower_item + upper_item
+                else:
+                    if upper_dict_priority < lower_dict_priority:
+                        upper_dict[item_key] = lower_item
+                    else:
+                        if not item_key in upper_dict:
+                            upper_dict[item_key] = lower_item
 
-                    if type(lower_data) is dict:
-                        _merge_dict(upper_data, 0, lower_data, 0)
+        _merge_dict_recursive(self.data, 0, lower_dict, 0)
 
-    result = {}
+    def _clear_dict(self):
+        def _clear_dict_recursive(dirty_dict):
+            for key in [k for k in dirty_dict.keys() if (type(k) is str) and k.startswith('__')]:
+                del dirty_dict[key]
 
-    for file_result in reversed(list(file_data.values())):
-        _merge_dict(result, 0, file_result, 0)
+            for key, item in dirty_dict.items():
+                if type(item) is dict:
+                    _clear_dict_recursive(item)
 
-    _clear_dict(result)
+        _clear_dict_recursive(self.data)
 
-    return result
+    def __init__(self, configuration_paths):
+        from collections import defaultdict
+
+        layers = []
+
+        for configuration_path in configuration_paths:
+            layers.append(_ConfigurationLayer(configuration_path))
+
+        file_data = defaultdict(list)
+        file_skip = set()
+
+        for layer in reversed(layers):
+            for file_entry in layer.file_entries:
+                if not file_entry.relative_path in file_skip:
+                    file_datum = file_entry.read()
+
+                    if type(file_datum) is dict:
+                        file_data[file_entry.relative_path].append(file_datum)
+                    else:
+                        file_skip.add(file_entry.relative_path)
+
+        self.data = {}
+
+        for file_data in reversed(list(file_data.values())):
+            for file_datum in file_data:
+                self._merge_dict(file_datum)
+
+        self._clear_dict()
+
+
+def merge(configuration_paths):
+    configuration = Configuration(configuration_paths)
+
+    return configuration.data
--- examples/brooms/test_configuration/test_configuration.py
+++ examples/brooms/test_configuration/test_configuration.py
@@ -4,14 +4,14 @@ from os.path import dirname
 from os.path import join
 from pprint import pprint
 
-from broomhilda.brooms.configuration import load
+from broomhilda.brooms.configuration import Configuration
 
 base_path = dirname(__file__)
 
-configuration = load([
+configuration = Configuration([
     join(base_path, './level-01'),
     join(base_path, './level-02'),
     join(base_path, './level-03'),
 ])
 
-pprint(configuration)
+pprint(configuration.data)
